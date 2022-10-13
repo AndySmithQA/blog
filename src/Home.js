df6b77e244d65ae070c3c6c32a823dfad98c718a
AndySmithQA <andy.smtih@qa.com>
2022-10-13 11:20:05 +0100

    outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+  const [blogs, setBlogs] = useState([
+    {title: 'My first blog', body: 'lorem', author: 'mario', id: 1},
+    {title: 'My first blog', body: 'lorem', author: 'Yoshi', id: 2},
+    {title: 'My Second blog', body: 'lorem', author: 'Mario', id: 3}
+  ]);
+  
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
