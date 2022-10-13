569714fa04d6ab510c0a4cb0ca354bba09a2b8d3
AndySmithQA <andy.smtih@qa.com>
2022-10-13 12:00:03 +0100

    UseEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,9 @@ const Home = () => {
     {title: 'My first blog', body: 'lorem', author: 'Yoshi', id: 2},
     {title: 'My Second blog', body: 'lorem', author: 'mario', id: 3}
   ]);
-  
+
+  const [name, setName] = useState('Mario');
+
   const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id != id);
         setBlogs(newBlogs);
@@ -17,11 +19,13 @@ const Home = () => {
   useEffect(() => {
     console.log('Use effect Ran');
     console.log(blogs);
-  });
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Luigi')}>Change name</button>
+      <p>{ name }</p>
       
       </div>
       )
